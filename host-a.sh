6d4a29243e1d5f1a3092cfc2eef1a43ac502c01d
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-10-07 22:18:31 +0200

    Add a shell file for every virtual machine

--- host-a.sh
+++ host-a.sh
@@ -1,2 +1,1 @@
 export DEBIAN_FRONTEND=noninteractive
-# Startup commands go here
--- switch.sh
+++ switch.sh
@@ -2,5 +2,3 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump
 apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-certificates curl software-properties-common
-
-# Startup commands for switch go here
