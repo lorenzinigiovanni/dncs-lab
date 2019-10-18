4055905d2bb9c729c9989ae7640338fed2bdb115
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-10-18 20:32:18 +0200

    Add shell commands for switch provision

--- switch.sh
+++ switch.sh
@@ -2,3 +2,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump
 apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-certificates curl software-properties-common
+
+ovs-vsctl add-br br0
+
+ovs-vsctl add-port br0 enp0s8
+ovs-vsctl add-port br0 enp0s9 tag=1
+ovs-vsctl add-port br0 enp0s10 tag=2
+
+ip link set enp0s8 up
+ip link set enp0s9 up
+ip link set enp0s10 up
