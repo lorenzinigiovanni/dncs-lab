172b092d567ff5fd98506f3787a5cfa28911f6f0
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-10-18 21:47:51 +0200

    Add routes to router-1

--- router-1.sh
+++ router-1.sh
@@ -1,5 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
+sysctl net.ipv4.ip_forward=1
+
 ip link add link enp0s8 name enp0s8.1 type vlan id 1
 ip link add link enp0s8 name enp0s8.2 type vlan id 2
 
@@ -9,3 +11,5 @@ ip addr add 192.168.2.1/24 dev enp0s8.2
 
 ip link set enp0s9 up
 ip link set enp0s8 up
+
+ip route add 192.168.3.0/25 via 192.168.3.130
