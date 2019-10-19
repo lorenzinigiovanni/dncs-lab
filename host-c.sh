83518e71b4946a338676d5ace66ec643627d2416
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-10-19 12:08:11 +0200

    Install docker on host-c

--- host-c.sh
+++ host-c.sh
@@ -1,5 +1,12 @@
 export DEBIAN_FRONTEND=noninteractive
 
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt install -y docker-ce
+
 ip addr add 192.168.3.2/25 dev enp0s8
 
 ip link set enp0s8 up
