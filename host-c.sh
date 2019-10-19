b4502c610b5c09985ee2c05587b7dd61ce55064e
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-10-19 12:09:25 +0200

    Create html file to serve

--- host-c.sh
+++ host-c.sh
@@ -9,6 +9,9 @@ apt install -y docker-ce
 
 docker pull nginx
 
+mkdir /www
+echo -e '<!DOCTYPE html>\n<html>\n<head>\n    <meta charset="UTF-8">\n    <title>DNCS LAB</title>\n</head>\n<body>\n    <h1>DNCS LAB</h1>\n    <h3>Author: Lorenzini Giovanni</h3>\n    <h3>Student number: 193473</h3>\n</body>\n</html>' > /www/index.html
+
 ip addr add 192.168.3.2/25 dev enp0s8
 
 ip link set enp0s8 up
