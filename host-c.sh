792e1cce2fc24eabd2d232daba68e2abb9b19c52
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-10-23 11:25:54 +0200

    Use dustnic82 nginx docker image

--- host-c.sh
+++ host-c.sh
@@ -7,12 +7,12 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bi
 apt update
 apt install -y docker-ce
 
-docker pull nginx
+docker pull dustnic82/nginx-test
 
 mkdir /www
 echo -e '<!DOCTYPE html>\n<html>\n<head>\n    <meta charset="UTF-8">\n    <title>DNCS LAB</title>\n</head>\n<body>\n    <h1>DNCS LAB</h1>\n    <h3>Author: Lorenzini Giovanni</h3>\n    <h3>Student number: 193473</h3>\n</body>\n</html>' > /www/index.html
 
-docker run --name nginx -v /www:/usr/share/nginx/html -d -p 80:80 nginx
+docker run --name nginx -v /www:/usr/share/nginx/html -d -p 80:80 dustnic82/nginx-test
 
 ip addr add 192.168.3.2/25 dev enp0s8
 
