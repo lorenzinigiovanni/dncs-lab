2c9e433654822ba3153ceb661056fa4572b8820d
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-11-03 17:32:15 +0100

    Use DHCP in Host-A subnet

--- router-1.sh
+++ router-1.sh
@@ -1,5 +1,13 @@
 export DEBIAN_FRONTEND=noninteractive
 
+apt update
+apt install -y isc-dhcp-server
+
+echo -e 'INTERFACESv4="enp0s8.1"\nINTERFACESv6=""' > /etc/default/isc-dhcp-server
+echo -e 'default-lease-time 600;\nmax-lease-time 7200;\nddns-update-style none;\nsubnet 192.168.1.0 netmask 255.255.255.0 {\n        range 192.168.1.2 192.168.1.254;\n        option subnet-mask 255.255.255.0;\n        option routers 192.168.1.1;\n}' > /etc/dhcp/dhcpd.conf
+
+systemctl start isc-dhcp-server.service
+
 sysctl net.ipv4.ip_forward=1
 
 ip link add link enp0s8 name enp0s8.1 type vlan id 1
